2d20149be6ef58d3084d492c503a73f5987d5993
yerin-158 <hanblueblue@gmail.com>
2020-06-19 14:16:22 +0900

    핸드셰이킹 인터셉터 추가, ServletServerContainerFactoryBean 추가

--- src/main/java/config/WebSocketConfig.java
+++ src/main/java/config/WebSocketConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import utils.MyHandler;
 
 @Configuration
@@ -13,11 +15,21 @@ import utils.MyHandler;
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "/myHandler");
+        registry.addHandler(myHandler(), "/myHandler") // 특정 URL에 웹소켓 핸들러를 매핑한다.
+                .addInterceptors(new HttpSessionHandshakeInterceptor()); // 핸드셰이크 요청을 인터셉트할 인터셉터
     }
 
     @Bean
     public WebSocketHandler myHandler(){
         return new MyHandler();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer(){
+        // WebSocket의 런타임 특성 제어
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
     }
 }
